0eec776243e1dded80152d9f049890f603d18440
ksh22p <74092309+ksh22p@users.noreply.github.com>
2025-12-30 15:48:42 +0900

    Refactor ReactDOM import and simplify root rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Could not find root element to mount to");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
